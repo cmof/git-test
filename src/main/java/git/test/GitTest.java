810dbdd02d34eedf2f2414050751c0124f27a9f2
ccy <1529939607@qq.com>
2022-04-11 20:45:12 +0800

    second commit

--- src/main/java/git/test/GitTest.java
+++ src/main/java/git/test/GitTest.java
@@ -7,5 +7,6 @@ package git.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git in IDEA");
+        System.out.println("Hello git1 in IDEA");
     }
 }
