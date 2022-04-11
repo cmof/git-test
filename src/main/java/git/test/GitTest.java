ac3ffc609d32015606df727d3f748cd747df9ed8
ccy <1529939607@qq.com>
2022-04-11 20:46:17 +0800

    third commit

--- src/main/java/git/test/GitTest.java
+++ src/main/java/git/test/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git in IDEA");
         System.out.println("Hello git1 in IDEA");
+        System.out.println("Hello git2 in IDEA");
     }
 }
