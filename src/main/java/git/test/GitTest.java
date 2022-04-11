85ba5752e34a1da90c0b32d8774ffd7d5a9c52fa
ccy <1529939607@qq.com>
2022-04-11 20:39:10 +0800

    first commit

---
+++ src/main/java/git/test/GitTest.java
@@ -0,0 +1,11 @@
+package git.test;
+
+/**
+ * @author ccy
+ * @create 2022/4/11 - 20:33
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git in IDEA");
+    }
+}
